20f07f5f99b848d2bec8afcb70de4318706f8864
Asshley <asshleydeanwaynegozar@gmail.com>
2024-06-24 22:07:36 +0800

    Updated LogIn Page

--- SourceCode/Credentials.java
+++ SourceCode/Credentials.java
@@ -2,7 +2,7 @@ package EnrollmentSystem.SourceCode;
 
 import java.util.HashMap;
 
-public class Credentials{
+abstract class Credentials{
 
    protected static HashMap<Integer, Object> studentCredentials = new HashMap<>();
    protected static HashMap<Integer, Object> adminCredentials = new HashMap<Integer, Object>();
--- SourceCode/LogInPage.java
+++ SourceCode/LogInPage.java
@@ -6,6 +6,10 @@ public class LogInPage extends Credentials{
     
     private static boolean loop = true;
     private static int input;
+    private static int adminAccountID;
+    private static Object adminPassword;
+    private static int studentAccountID;
+    private static Object studentPassword;
 
      static void userLogIn(Scanner logIn){
 
@@ -24,36 +28,92 @@ public class LogInPage extends Credentials{
 
                 switch(input){
 
-                 case 1 -> loop = false;
+                 case 1:
+                  loop = false;
+                  adminCredential(logIn);
+                  break;
+                
+                 case 2:
+                 loop = false;
+                 studentCredential(logIn);
+                 break;
 
-                 case 2 -> loop = false;
-
-                 default -> loop = true;   
-
-                 }   
-                   
+                 default:
+                   loop = true;   
+               }                   
             } catch (java.util.InputMismatchException e) {
                
                System.out.println("Integers Number Only!");
                break;
-            }
-            
-            
+            }   
         } 
     }
 
-    static void adminCredentials(Scanner logIn){
+    static void adminCredential(Scanner logIn){
 
+        try {
         
-
-        System.out.print("Account ID: ");
-        int accountID = logIn.nextInt();
+            System.out.print("Account ID: ");
+            adminAccountID = logIn.nextInt();
                         logIn.nextLine();
 
-        System.out.println("Password: ");
-        Object password = logIn.nextLine();
+            System.out.print("Password: ");
+            adminPassword = logIn.nextLine();
 
-    }
+       } catch (Exception e) {
+
+            System.out.println("Accound ID should be Integer Only!");
+        
+         } 
+            if(!adminCredentials.containsKey(adminAccountID)){
+               System.out.println("Account ID does not Exist!");
+               adminCredential(logIn);
+
+              }else{ 
+                   
+                    if(!adminCredentials.containsValue(adminPassword)){
+                        System.out.println("Incorrect password!");
+                        adminCredential(logIn);
+     
+                      }else{     
+                        System.out.println("Log In Succcessful!");
+                        System.out.println("Welcome to your dashboard user " + adminAccountID);     
+               } 
+            }  
+         }
+
+    static void studentCredential(Scanner logIn){
+
+        try {
+        
+            System.out.print("Account ID: ");
+            studentAccountID = logIn.nextInt();
+                        logIn.nextLine();
+
+            System.out.print("Password: ");
+            studentPassword = logIn.nextLine();
+
+       } catch (Exception e) {
+
+            System.out.println("Accound ID should be Integer Only!");
+        
+         } 
+            if(!studentCredentials.containsKey(studentAccountID)){
+               System.out.println("Account ID does not Exist!");
+               studentCredential(logIn);
+
+              }else{ 
+                   
+                    if(!studentCredentials.containsValue(studentPassword)){
+                        System.out.println("Incorrect password!");
+                        studentCredential(logIn);
+     
+                      }else{     
+                        System.out.println("Log In Succcessful!");
+                        System.out.println("Welcome to your dashboard user " + studentAccountID);     
+               } 
+            }
+         }
 
     public static void main(String[]args){
 
