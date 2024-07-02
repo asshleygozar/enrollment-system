4813ebc9aef6463b5a317aa5e10925abf868a9d9
Asshley <asshleydeanwaynegozar@gmail.com>
2024-07-02 08:49:53 +0800

    Enrollment Management Class Updated!

--- SourceCode/EnrollmentManagement.java
+++ SourceCode/EnrollmentManagement.java
@@ -2,6 +2,7 @@ package EnrollmentSystem.SourceCode;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Random;
 
 public class EnrollmentManagement {
@@ -86,7 +87,7 @@ public class EnrollmentManagement {
              break;
     
             case 3:
-             adminRemoveEnrollee();
+             adminRemoveEnrollee(enroll);
              looping = false;
              break;
     
@@ -119,44 +120,50 @@ public class EnrollmentManagement {
           System.out.println("Gender: " + studentGenderIdentity.get(i)); 
           System.out.println("");
         }
-
+      
        while(looping){
-          
-        System.out.println("Choose your next Action:\n");
+
+        try {
+          System.out.println("Choose your next Action:\n");
         
-        System.out.println("1. Enroll a Student");
-        System.out.println("2. View Enrollees");
-        System.out.println("3. Remove a Student");
-        System.out.println("4. Exit\n");
-
-        System.out.print("Enter here: ");
-        int enter = sc.nextInt();
-
-        switch (enter) {
-
-          case 1:
-            adminEnrollment(sc);
-            looping = false;
-             break;
-
-          case 2:
-            adminViewEnrollees(sc);
-            looping = false;
-            break;
-      
-          case 3:
-            adminRemoveEnrollee();
-            looping = false;
-            break;
-            
-          case 4:
-            new LogInPage(sc);
-            looping = false;
-            break;
-      
-          default:
-           looping = true;
-      
+          System.out.println("1. Enroll a Student");
+          System.out.println("2. View Enrollees");
+          System.out.println("3. Remove a Student");
+          System.out.println("4. Exit\n");
+  
+          System.out.print("Enter here: ");
+          int enter = sc.nextInt();
+  
+          switch (enter) {
+  
+            case 1:
+              adminEnrollment(sc);
+              looping = false;
+               break;
+  
+            case 2:
+              adminViewEnrollees(sc);
+              looping = false;
+              break;
+        
+            case 3:
+              adminRemoveEnrollee(sc);
+              looping = false;
+              break;
+              
+            case 4:
+              new LogInPage(sc);
+              looping = false;
+              break;
+        
+            default:
+             looping = true;
+        
+           }  
+        } catch (InputMismatchException e) {
+           
+           System.out.println("Invalid Input!");
+           break;
           }  
        }
    }
@@ -164,9 +171,96 @@ public class EnrollmentManagement {
      
      
 
-   public void adminRemoveEnrollee(){
+   public void adminRemoveEnrollee(Scanner scan){
     
+     System.out.println("Remove Enrollee Section");
 
+     for(int i = 0; i < studentID.size(); i++){
+      System.out.println( "Student ID: " + studentID.get(i));
+      System.out.println("Student Name: " + studentName.get(i));
+      System.out.println("Age: " + studentAges.get(i));
+      System.out.println("Gender: " + studentGenderIdentity.get(i)); 
+      System.out.println("");
+    
+      }     
+
+      while(looping){
+         try {
+          System.out.print("Enter the Student ID: ");
+          int delete = scan.nextInt();
+
+          if(studentID.contains(delete)){
+             for(int d = 0; d < studentID.size(); d++){
+                
+               if(studentID.get(d) == delete){
+
+                   studentID.remove(studentID.get(d));
+                   studentName.remove(studentName.get(d));
+                   studentAges.remove(studentAges.get(d));
+                   studentGenderIdentity.remove(studentGenderIdentity.get(d));
+                   break;
+               }
+             }
+            System.out.println("Student Removed Successfully!");
+
+            for(int i = 0; i < studentID.size(); i++){
+              System.out.println( "Student ID: " + studentID.get(i));
+              System.out.println("Student Name: " + studentName.get(i));
+              System.out.println("Age: " + studentAges.get(i));
+              System.out.println("Gender: " + studentGenderIdentity.get(i)); 
+              System.out.println("");
+        
+               }   
+            }else{
+               System.out.println("Student ID does not exists!");
+            }
+
+         } catch (Exception e) {
+            System.out.println("Invalid Student ID Input!");
+            break;
+         }
+
+         try {
+          System.out.println("Choose your next action:\n");
+          System.out.println("1. Enroll new student");
+          System.out.println("2. View Enrollees");
+          System.out.println("3. Remove a student");
+          System.out.println("4. Exit");
+ 
+          System.out.print("Enter here: ");
+          int input = scan.nextInt();
+ 
+          switch (input) {
+   
+           case 1:
+             adminEnrollment(scan);
+             looping = false;
+              break;
+ 
+           case 2:
+             adminViewEnrollees(scan);
+             looping = false;
+             break;
+       
+           case 3:
+             adminRemoveEnrollee(scan);
+             looping = false;
+             break;
+             
+           case 4:
+             new LogInPage(scan);
+             looping = false;
+             break;
+       
+           default:
+            looping = true;
+       
+            }
+         } catch (Exception e) {
+           System.out.println("Invalid Input!");
+           break;
+         } 
+      }
    }
    
    public void adminManagement(Scanner scanner){
@@ -184,31 +278,31 @@ public class EnrollmentManagement {
         System.out.print("Enter here: ");
         userInput = scanner.nextInt();
 
+        switch(userInput){
+
+          case 1: 
+           adminEnrollment(scanner);
+           looping = false;
+           break;
+
+          case 2:
+           adminViewEnrollees(scanner);
+           looping = false;
+           break;
+
+          case 3:
+           adminRemoveEnrollee(scanner);
+           looping = false;
+           break;
+
+          default:
+           looping = true;
+       }
+
     } catch (java.util.InputMismatchException e) {
   
           System.out.println("Invalid Input!");
           break;
-         }
-
-         switch(userInput){
-
-            case 1: 
-             adminEnrollment(scanner);
-             looping = false;
-             break;
-
-            case 2:
-             looping = false;
-             adminViewEnrollees(scanner);
-             break;
-
-            case 3:
-             looping = false;
-             adminRemoveEnrollee();
-             break;
-
-            default:
-             looping = true;
          }
       } 
    }
