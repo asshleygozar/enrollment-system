2cee233b513d4900a32238c70b7ed7cd7c6b9152
Asshley <asshleydeanwaynegozar@gmail.com>
2024-07-09 14:16:59 +0800

    Credentials Class Modified

--- SourceCode/Credentials.java
+++ SourceCode/Credentials.java
@@ -6,6 +6,6 @@ abstract class Credentials{
 
    protected static HashMap<Integer, Object> studentCredentials = new HashMap<Integer, Object>();
    protected static HashMap<Integer, Object> adminCredentials = new HashMap<Integer, Object>();
-
+   
 
 }
