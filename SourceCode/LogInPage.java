379c9caee87e83354548da60fdcd7d579a5761a7
Asshley <asshleydeanwaynegozar@gmail.com>
2024-07-09 14:16:20 +0800

    Log In Page Class Modified

--- SourceCode/LogInPage.java
+++ SourceCode/LogInPage.java
@@ -1,7 +1,10 @@
 package EnrollmentSystem.SourceCode;
 
+import java.util.InputMismatchException;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.io.*;
 
 public class LogInPage extends Credentials{
     
@@ -11,15 +14,44 @@ public class LogInPage extends Credentials{
     private  Object adminPassword;  // admin password scanner
     private  int studentAccountID;  // student accound ID scanner
     private  Object studentPassword;  // student password scanner
+    private int minimumID = 6;       //Minimum 6 characters for ID 
+    private int minimumPassword = 8; // Minimum of 8 Characters for Password 
+    final static String outputFilePath = "G:\\Data Information Files\\AdminCredentials.txt"; //File path for Admin Credentials
 
-  
+   // Asks the user if they are going to log in or register
+     public void userPage(Scanner scan){
+        
+        while(loop){
+            try {
+                System.out.println("Please choose your action:");
+
+                System.out.println("1. Register");
+                System.out.println("2. Log In");
+
+                System.out.print("Enter here: ");
+                int input = scan.nextInt();
+
+                switch(input){
+
+                    case 1 -> userRegister(scan);
+
+                    case 2 -> userLogIn(scan);
+
+                    default -> loop = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid Input!");
+                break;
+            }
+        }
+    }
     // Asks the user what is their role
      public void userLogIn(Scanner logIn){
 
         while(loop){
             
             try {
-   
+                System.out.println("Welcome to Log In Page!");
                 System.out.println(" What is your role?");
 
                 System.out.println("1.  Admin");
@@ -50,6 +82,37 @@ public class LogInPage extends Credentials{
                break;
             }   
         } 
+    }
+      // Registration Method
+    public void userRegister(Scanner scan){
+       
+        while(true){
+
+             try {
+                
+                System.out.println("Welcome to Registration Page!");
+
+                System.out.print("Please Enter your School ID: ");
+                int registerID = scan.nextInt();
+
+                System.out.print("Please Enter your Password: ");
+                String registerPassword = scan.nextLine();
+
+                if(registerID < minimumID){
+                  System.out.println("Minimum of 6 characters for School ID");
+                 }else if(registerPassword.length() < minimumPassword){
+                   System.out.println("Minimum of 8 Characters for password!");
+                   }else{
+                    adminCredentials.put(registerID,registerPassword);
+                    System.out.println("Registered Successful!");
+                    userLogIn(scan);
+                     break;
+                 }
+             } catch (InputMismatchException e) {
+                System.out.println("Invalid ID Input!");
+                break;
+            }
+        }  
     }
    // if user is admin, they will go here. The admin login 
     public void adminCredential(Scanner logIn){
@@ -132,7 +195,7 @@ public class LogInPage extends Credentials{
             }
          }
 
-    public static void main(String[]args){
+    public static void main(String[]args) {
     Scanner scan =  new Scanner(System.in);
        // Data Placeholder using Hashmap 
         adminCredentials.put(54321, "fefri");
@@ -142,8 +205,36 @@ public class LogInPage extends Credentials{
 
         studentCredentials.put(2023, "OuM");
         studentCredentials.put(2024, "Yes");
+   
+        File file = new File(outputFilePath);
+        
+        BufferedWriter bf = null;
 
-        new LogInPage().userLogIn(scan);;
+         try {
+            
+            bf  = new BufferedWriter(new FileWriter(file));
+
+             for(Entry<Integer, Object> entry : adminCredentials.entrySet()){
+                 bf.write(entry.getKey() + ":" + entry.getValue());
+
+                 // new Line
+                  bf.newLine();
+             }
+              bf.flush();
+         } catch (IOException e) {
+             e.printStackTrace();
+
+
+         }finally{
+
+            try{
+                //always close the writer
+                bf.close();
+            }catch(Exception e){
+
+             }
+         }
+        new LogInPage().userPage(scan);
 
     }
 }
