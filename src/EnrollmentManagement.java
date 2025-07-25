48e68967e30eef197113cf8fc8f94e78474c3bf5
Asshley <asshleydeanwaynegozar@gmail.com>
2025-07-25 23:03:03 +0800

    rename folder to src for conciseness

--- src/EnrollmentManagement.java
+++ src/EnrollmentManagement.java
@@ -1,4 +1,4 @@
-package EnrollmentSystem.SourceCode;
+
 
 import java.util.Scanner;
 import java.util.ArrayList;
--- src/LogInPage.java
+++ src/LogInPage.java
@@ -1,4 +1,4 @@
-package EnrollmentSystem.SourceCode;
+
 
 import java.util.InputMismatchException;
 import java.util.Map;
